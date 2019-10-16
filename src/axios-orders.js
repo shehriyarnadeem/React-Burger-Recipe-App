a58a1f2651f83a106589e54a298cabfcb7cec325
Shehriyar Nadeem <shehriyarnadeem@Shehriyars-MacBook-Pro.local>
2019-10-16 19:08:08 +0500

    Url changed

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://react-my-burger.firebaseio.com/'
+  baseURL: 'https://burger-builder-82297.firebaseio.com/'
 });
 
 export default instance;
